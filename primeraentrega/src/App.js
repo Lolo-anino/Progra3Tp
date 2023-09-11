9aa1713d4f5d1783bec904ec920f825caf435c5e
Lorenzo Cozzo <lcozzo@udesa.edu.ar>
2023-09-11 10:39:44 -0300

    lonks

--- primeraentrega/src/App.js
+++ primeraentrega/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from './logo.svg';
 import './App.css';
 import { Link, Route, Switch} from 'react-router-dom';
 import Header from './components/Header/Header';
--- primeraentrega/src/components/Footer/Footer.jsx
+++ primeraentrega/src/components/Footer/Footer.jsx
@@ -5,8 +5,8 @@ function Footer() {
 return(
 <footer>
         <div class="footer">
-            <link to="index.html"><h3 class="nombre">CUARTETOFY</h3></link>
-            <link to="index.html"><img src="./img/logo.jpg" alt="" class="logo"/></link>
+            <link to="/"><h3 class="nombre">CUARTETOFY</h3></link>
+            <link to="/"><img src="./img/logo.jpg" alt="" class="logo"/></link>
             <link to="instagram.com"><i class="fa-brands fa-instagram"></i></link>
             <link to="twitter.com"><i class="fa-brands fa-twitter"></i></link>
             <link to="facebook.com"><i class="fa-brands fa-facebook-square"></i></link>
