0185f7c75afd8bc741014522adc671549a3315d6
Matias Roccia <mroccia@udesa.edu.ar>
2023-09-12 16:14:47 -0300

    detalle musica y otras cositis

--- primeraentrega/src/App.js
+++ primeraentrega/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
-import './App.css';
-import { Link, Route, Switch} from 'react-router-dom';
+
+import { Route, Switch} from 'react-router-dom';
 import Header from './components/Header/Header';
 import Index from './components/Index/Index';
 import NotFound from './components/NotFound/NotFound';
+import MusicDetail from "./screens/MusicDetail/MusicDetail";
 import Footer from './components/Footer/Footer';
 import Todas from './components/Todas/Todas'
 
@@ -20,6 +21,7 @@ function App() {
         <Route path='/' exact={true} component={Index} />
         <Route path='' component={NotFound} />
         <Route path='/all' component={Todas} />
+        <Route path='/music/id:id' component={MusicDetail} />
       </Switch>
     </main>
     <Footer />
--- primeraentrega/src/components/Header/Header.jsx
+++ primeraentrega/src/components/Header/Header.jsx
@@ -4,8 +4,8 @@ import {Link} from "react-router-dom"
 function Header(props) {
     return (
         <header className="cabecera">
-        <Link to="/" class="h1"><h1>CUARTETOFY</h1></Link>
-        <Link to="/"><img src="" alt="" className="logo"/></Link>
+        <Link to="/" className="h1"><h1>CUARTETOFY</h1></Link>
+        <Link to="/"><img src="/logo.jpg" alt="" className="logo"/></Link>
             <div>
                 <form action="./detalles-busqueda.html" method="get" >
                     <input name="buscarr" type="search" placeholder="Buscar" className="buscar"/>
