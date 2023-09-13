3b3b5b756d1b74001e906f4452eecc1c148a855d
Lorenzo Cozzo <lcozzo@udesa.edu.ar>
2023-09-13 09:47:58 -0300

    p4

--- primeraentrega/src/components/Todas/Todas.js
+++ primeraentrega/src/components/Todas/Todas.js
@@ -1,16 +1,24 @@
-import React from "react"
-
-function Todas(props){
-    return(
-        <nav>
-            <ul className="cantantes">
-                <h2>Ver todas</h2>
-            
-
-    
-            </ul>
-        </nav>
+import React, {Component} from "react";
+import "../../App.css";
+import loadGif from "../../loadGif.gif";
+class Todas extends Component{
+componentDidMount(){
+fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart`)
+.then(response => response.json() )
+.then(data => this.setState({
+    musicInfo: data})
     )
 }
+
+    render(){
+        return(
+            <div>
+                <h1 className="main-title">Ver todas</h1>
+
+            </div>
+        )
+    }
+}
+
 
 export default Todas
