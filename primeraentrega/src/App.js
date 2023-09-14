0f1058a137929ffa8796a4ae801bae625b310df8
Lorenzo Cozzo <lcozzo@udesa.edu.ar>
2023-09-14 20:10:11 -0300

    p4 alargado

--- primeraentrega/src/App.js
+++ primeraentrega/src/App.js
@@ -6,7 +6,8 @@ import Index from './components/Index/Index';
 import NotFound from './components/NotFound/NotFound';
 import MusicDetail from "./screens/MusicDetail/MusicDetail";
 import Footer from './components/Footer/Footer';
-import Todas from './components/Todas/Todas';
+import Todas from './components/Todas/Canciones';
+import Todos from './screens/Todas/Albumes';
 /* import DetalleCancion from './components/DetalleCancion/DetalleCancion';
 import DetalleAlbum from './components/DetalleAlbum/DetalleAlbum'; */
 
@@ -24,7 +25,8 @@ function App() {
         <Route path='/search-result/:busqueda' exact={true} component={Index} />
         {/* <Route path='/album/id/:id' component={DetalleAlbum}/> */}
        {/* <Route path='/cancion/id/:id' component={DetalleCancion}/> */} 
-        <Route path='/all' component={Todas} />
+        <Route path='/allsongs' component={Todas} />
+        <Route path='/allalbums' component={Todos} />
         <Route path='/music/id:id' component={MusicDetail} />
         <Route path='' component={NotFound} />
       </Switch>
