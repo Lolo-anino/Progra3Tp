cd2bd32ec4694a2c41ef9cd6cb528b015d1fc3fe
lolo-anino <lanino@udesa.edu.ar>
2023-09-15 13:24:15 -0300

    cambios en cancion

--- primeraentrega/src/components/Index/Index.jsx
+++ primeraentrega/src/components/Index/Index.jsx
@@ -1,20 +1,50 @@
 import React, { Component } from 'react';
-import CardContainer from '../CardConteiner/CardConteiner';
+import CardContainer from '../CardContainer/CardContainer';
 
 class Index extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            arrayCanciones : [],
+            arrayAlbums : []
+
+        }
+    }
+    componentDidMount() {
+        let urlCanciones =
+        `https://thingproxy.freeboard.io/fetch/https://api.deezer.com/chart/0/tracks?index=0&limit=5`;
+        let urlAlbums =
+          `https://thingproxy.freeboard.io/fetch/https://api.deezer.com/chart/0/albums?index=0&limit=5`;
+        fetch(urlCanciones)
+          .then((response) => response.json())
+          .then((data) =>
+            this.setState({
+              arrayCanciones: data.results,
+            })
+          )
+          .catch((e) => console.log(e));
+        fetch(urlAlbums)
+          .then((response) => response.json())
+          .then((data) =>
+            this.setState({
+              arrayAlbum: data.results,
+            })
+          )
+          .catch((e) => console.log(e));
+      }
     render(){
         return(
-        <nav>
+        <div>
             <img src="./img/banner.jpg" alt="" class="banner"></img>
-            <main class="cantantes">
+            <main className="cantantes">
                 <h2>Canciones</h2>
-                <CardContainer url="https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart"/>
+                <CardContainer arrayCancion={this.state.arrayCanciones} esCacion ={true}/>
                 </main>
-                <main class="cantantes">
-                <h2>Generos</h2>
-                <CardContainer url="https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart"/>
+                <main className="cantantes">
+                <h2>Generos</h2>{/* 
+                <CardContainer arrayAlbum={this.state.arrayAlbums} esCacion ={false}/> */}
             </main>
-        </nav>
+        </div>
     )}
 }
 
